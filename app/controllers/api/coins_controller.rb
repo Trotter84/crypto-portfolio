9ec0742fd06c92cd3690d21feb3086c9735d64c3
Daniel Trotter <flare84@gmail.com>
2018-10-08 12:36:36 -0600

    final server code

--- app/controllers/api/coins_controller.rb
+++ app/controllers/api/coins_controller.rb
@@ -1,11 +1,19 @@
 class Api::CoinsController < ApplicationController
-  before_action :set_coin, only: [:show, :update, :destroy]
+  before_action :set_coin, only: [:update, :destroy]
   BASE_URL = 'https://api.coinmarketcap.com/v2/'
 
   def index
+    listings = HTTParty.get("#{BASE_URL}ticker")['data'].values
+    user_coins = current_user.coins
+    user_coins.each do |coin|
+      res_coin = listings.find { |c| c['id'] === coin.cmc_id }
+      coin.update(price: res_coin['quotes']['USD']['price'])
+    end
+
     render json: current_user.coins
   end
 
+  # POST /api/coins ?coin=btc
   def create
     cmc_id = params[:coin].upcase
     listings = HTTParty.get("#{BASE_URL}listings")
@@ -21,9 +29,12 @@ class Api::CoinsController < ApplicationController
   end
 
   def show
-    render json: @coin
+    res = HTTParty.get("#{BASE_URL}ticker/#{params[:id]}")
+    render json: res['data']
   end
 
+  # PUT /api/coins/:id
+  # Stop watching a coin
   def update
     current_user.watched_coins.find_by(coin_id: @coin.id).destroy
   end
@@ -36,5 +47,6 @@ class Api::CoinsController < ApplicationController
     def set_coin
       @coin = Coin.find(params[:id])
     end
+
 
 end
